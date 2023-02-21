fbd5d89f056c1781622cae4188659ba016076be4
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-02-20 22:49:57 -0700

    2

--- lib/supabaseClient.tsx
+++ lib/supabaseClient.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { createClient } from '@supabase/supabase-js'
-import { Database } from 'types/database'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
