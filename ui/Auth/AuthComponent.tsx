ccee9812c12e1b0e97827600d9ac850e4515a529
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-03 18:54:04 -0700

    readd supa

--- ui/Auth/AuthComponent.tsx
+++ ui/Auth/AuthComponent.tsx
@@ -1,6 +1,4 @@
 "use client";
-
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { signIn } from "next-auth/react";
 import { toast } from "react-toastify";
 import { ConnectToSubport } from "ui/Buttons/ConnectButton";
