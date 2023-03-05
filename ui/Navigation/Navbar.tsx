51bbff957950d6efdfe98befbe1ee1f103c109e0
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-04 22:54:27 -0700

    svg test

--- ui/Navigation/Navbar.tsx
+++ ui/Navigation/Navbar.tsx
@@ -88,7 +88,7 @@ function Navbar() {
                 <svg
                  
                   stroke="white"
-                  strokeWidth={1.5}
+                  strokeWidth='1.5'
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                   aria-hidden="true"
