431f1dce1e81d696ed7ce7b8655e17040468d8e6
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-03-30 02:17:11 -0700

    ipfs test

--- ui/Sections/Create/CreateForm.tsx
+++ ui/Sections/Create/CreateForm.tsx
@@ -3,9 +3,35 @@ import React, { useState } from "react";
 import Collectible from "types/collectible";
 import { toast } from "react-toastify";
 import { useForm } from "react-hook-form";
-import { uploadToIpfs } from "lib/hooks/IPFSUpload"; 
 import { Media } from "ui/Misc/Media";
 
+import { create } from "ipfs-http-client";
+export const uploadToIpfs = async (imageFile: any, audioFile: any) => {
+    const projectId = process.env.NEXT_PUBLIC_INFURA_ID;
+    const projectSecret = process.env.NEXT_PUBLIC_INFURA_SECRET;
+    const auth =
+      "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
+    const ipfs =  create({
+      timeout: "2m",
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+      headers: {
+        authorization: auth,
+      },
+    });
+  
+    // Upload image file to IPFS
+    const imageResult = await ipfs.add(imageFile);
+    const imageUrl = `ipfs://${imageResult.path}`;
+  
+    // Upload audio file to IPFS
+    const audioResult = await ipfs.add(audioFile);
+    const audioUrl = `ipfs://${audioResult.path}`;
+  
+    return { image: imageUrl, audio: audioUrl };
+  };
+  
 
 export const CreateForm = () => {
   const [audioUrl, setAudioUrl] = useState();
