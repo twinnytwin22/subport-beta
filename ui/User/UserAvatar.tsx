5056835dac957b298df97ff61dfccddc5535ae69
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-02 14:29:42 -0700

    Update UserAvatar.tsx

--- ui/User/UserAvatar.tsx
+++ ui/User/UserAvatar.tsx
@@ -1,26 +1,44 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import UserMenu from './UserMenu';
 import { useSession } from 'next-auth/react';
+
 function UserAvatar() {
-    const [isOpen, setIsOpen] = useState(false);
-    console.log(isOpen, 'open:')
-    const {data:session, status} = useSession()
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session, status } = useSession();
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !(menuRef.current as HTMLElement).contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuRef]);
+
+  const toggleMenu = () => {
+   
+    {isOpen == false ? setIsOpen(true) : setIsOpen(false)};
+  };
+
   return (
-    <>
-    <div onMouseEnter={() => setIsOpen(true)}
-         onClick={() => setIsOpen(false)}>
-        <img
-    className="flex w-10 rounded-full"
-    src={session?.user?.image as string}
-    alt="Rounded avatar"
-  /></div>
-  {isOpen && 
-  <div className='absolute z-50 top-16 right-5 max-w-8xl'>
-  <UserMenu/></div>
-  }
-  </>
-  )
+    <div className="relative">
+      <div onClick={toggleMenu} className="block w-10 rounded-full cursor-pointer">
+        <img className="block w-full" src={session?.user?.image as string} alt="avi" />
+      </div>
+      {isOpen && (
+        <div ref={menuRef} className="absolute z-50 top-12 right-0">
+          <UserMenu />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default UserAvatar
+export default UserAvatar;
