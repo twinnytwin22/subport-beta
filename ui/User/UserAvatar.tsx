7a90242b1f5bdf5e69b579606122bf53b7d7fcf0
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-02 14:36:33 -0700

    avaatar update

--- ui/User/UserAvatar.tsx
+++ ui/User/UserAvatar.tsx
@@ -28,9 +28,9 @@ function UserAvatar() {
   };
 
   return (
-    <div className="relative">
-      <div onClick={toggleMenu} className="block w-10 rounded-full cursor-pointer">
-        <img className="block w-full" src={session?.user?.image as string} alt="avi" />
+    <div className="relative rounded-full bg-blue-900">
+      <div onClick={toggleMenu} className="block w-10 bg-blue-900 rounded-full cursor-pointer">
+        <img className="block w-full bg-blue-800 rounded-full" src={session?.user?.image as string} alt="avi" />
       </div>
       {isOpen && (
         <div ref={menuRef} className="absolute z-50 top-12 right-0">
