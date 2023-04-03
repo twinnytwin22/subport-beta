0996721e75d1293195396cd73cf0b588cb865bec
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-03 00:50:29 -0700

    update menu card

--- ui/Navigation/CardMenu.tsx
+++ ui/Navigation/CardMenu.tsx
@@ -1,5 +1,12 @@
+'use client'
 import React from 'react'
 import { signOut } from 'next-auth/react'
+import { toast } from 'react-toastify';
+
+const handleLogout = async () => {
+  toast("Signing Out");
+  signOut();
+};
 function CardMenu() {
   return (
     <div className='hidden md:block'>
@@ -7,8 +14,7 @@ function CardMenu() {
 <ul className="w-48 text-sm font-medium text-zinc-900 bg-white border border-zinc-200 rounded-lg dark:bg-zinc-900 dark:border-zinc-700 dark:text-white opacity-90">
     <li className="w-full px-4 py-2 border-b border-zinc-200 rounded-t-lg dark:border-zinc-600 hover:dark:bg-zinc-700">Profile</li>
     <li className="w-full px-4 py-2 border-b border-zinc-200 dark:border-zinc-600 hover:dark:bg-zinc-700">Settings</li>
-    <li className="w-full px-4 py-2 border-b border-zinc-200 dark:border-zinc-600 hover:dark:bg-zinc-700">Messages</li>
-    <div onClick={() => signOut()}>
+    <div onClick={handleLogout}>
     <li className="w-full px-4 py-2 rounded-b-lg hover:dark:bg-zinc-700">Sign out</li></div>
 </ul>
 
