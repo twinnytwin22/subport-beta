15c0092e10fc7f8abdc4115ae3d1acc199b16d4c
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-19 08:56:14 -0700

    Update CreateForm.tsx

--- ui/Sections/Create/CreateForm.tsx
+++ ui/Sections/Create/CreateForm.tsx
@@ -527,9 +527,9 @@ return (
       {step === 4 &&  <>
       <h1 className="text-center text-4xl">Creating your collectible</h1>
       </>}
-      {step === 1 && renderStep3()}
+      {step === 1 && renderStep1()}
       {step === 2 && renderStep2()}
-      {step === 3 && renderStep1()}
+      {step === 3 && renderStep3()}
       {step === 4 && renderMintStatusCard()}
     </div>
   );
