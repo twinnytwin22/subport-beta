8f30062d7b8d35afd2865cfbd2636cca4a2ea11e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-19 09:44:26 -0700

    Update CreateForm.tsx

--- ui/Sections/Create/CreateForm.tsx
+++ ui/Sections/Create/CreateForm.tsx
@@ -353,8 +353,7 @@ export const CreateForm = ({address}:any) => {
                   ></path>
                 </svg>
                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
+                  <span className="font-semibold">Click to upload</span>
                 </p>
                 <p className="text-xs text-gray-500 dark:text-gray-400">
                   PNG, JPG or GIF (MAX.50MB)
