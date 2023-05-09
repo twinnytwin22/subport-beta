c1b79beb721c9f0010cb4d39f7ae7647b0ef7567
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-05-08 21:08:33 -0700

    Update hardhat.config.ts

--- deploy/hardhat.config.ts
+++ deploy/hardhat.config.ts
@@ -1,31 +1,30 @@
-
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const polygonVerifyKey: any = process.env.POLYGONSCAN_API!
-const mumbaiApiKey: any = process.env.STAGING_ALCHEMY_KEY!
-const maticApiKey: any = process.env.PROD_ALCHEMY_KEY!
-const PK: any = process.env.PRIVATE_KEY!
+const polygonVerifyKey: any = process.env.POLYGONSCAN_API!;
+const mumbaiApiKey: any = process.env.STAGING_ALCHEMY_KEY!;
+const maticApiKey: any = process.env.PROD_ALCHEMY_KEY!;
+const PK: any = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
   paths: {
-		sources: "./contracts",
-		artifacts: "./artifacts",
-	  },
+    sources: "./contracts",
+    artifacts: "./artifacts",
+  },
   solidity: {
     version: "0.8.9",
     settings: {
-        optimizer: {
-            enabled: true,
-            runs: 2000, 
-            details: {
-              yul: true,
-              yulDetails: {
-                stackAllocation: true,
-                optimizerSteps: "dhfoDgvulfnTUtnIf",
-              },
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+        details: {
+          yul: true,
+          yulDetails: {
+            stackAllocation: true,
+            optimizerSteps: "dhfoDgvulfnTUtnIf",
+          },
         },
       },
     },
@@ -39,17 +38,12 @@ const config: HardhatUserConfig = {
   networks: {
     mumbai: {
       url: mumbaiApiKey,
-      accounts: PK,
+      accounts: [PK],
     },
     matic: {
       chainId: 137,
       url: maticApiKey,
-      accounts: PK,
-    },
-    mainnet: {
-      chainId: 1,
-      url: process.env.PROD_ALCHEMY_KEY as any,
-      accounts: [process.env.PRIVATE_KEY!] as any,
+      accounts: [PK],
     },
   },
 };
