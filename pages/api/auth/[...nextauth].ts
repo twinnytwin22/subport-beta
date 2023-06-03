fd35cf4b506cf65901df754aade1e63bf2e7d973
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-06-02 21:19:06 -0700

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,59 +1,60 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { NextAuthOptions } from "next-auth";
-import { getCsrfToken, getSession } from "next-auth/react";
 import SpotifyProvider from "next-auth/providers/spotify";
 import GoogleProvider from "next-auth/providers/google";
-import { SupabaseAdapter } from '@next-auth/supabase-adapter'
-import jwt from "jsonwebtoken"
+import { SupabaseAdapter } from "@next-auth/supabase-adapter";
+import jwt from "jsonwebtoken";
 import { supabase } from "lib/supabaseClient";
 import { generateWallet } from "lib/hooks/generateWallet";
+
 export function getAuthOptions(): NextAuthOptions {
   const providers = [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
     }),
   ];
+
   return {
     callbacks: {
       async session({ session, token, user }: any) {
-        const signingSecret = process.env.SUPABASE_JWT_SECRET
-        session.id = token?.sub
+        const signingSecret = process.env.SUPABASE_JWT_SECRET;
+        session.id = token?.sub;
+
         // Check if wallet_address exists for this user
         let { data, error } = await supabase
-          .from('users')
-          .select('wallet_address')
-          .eq('id', token.sub)
-          .limit(1)
-        
+          .from("users")
+          .select("wallet_address")
+          .eq("id", token?.sub)
+          .limit(1);
+
         if (error) {
-          throw new Error(error.message)
+          throw new Error(error.message);
         }
 
         // Generate a new wallet address for this user if it doesn't exist
         if (!data || !data.length || !data[0]?.wallet_address) {
-          const walletAddress = await generateWallet()
+          const walletAddress = await generateWallet();
           let { data, error } = await supabase
-            .from('users')
+            .from("users")
             .upsert({
               id: token.sub,
-              wallet_address: walletAddress.address
+              wallet_address: walletAddress.address,
             })
-            .eq('id', token.sub)
-        
+            .eq("id", token.sub);
+
           if (error) {
-            throw new Error(error.message)
+            throw new Error(error.message);
           }
           session.user = {
-            wallet_address: walletAddress,
             email: session?.user?.email,
             name: session?.user?.name,
             image: session?.user?.image,
-            id: session?.user?.id
+            id: session?.user?.id,
           };
         } else {
           session.user = {
@@ -61,7 +62,7 @@ export function getAuthOptions(): NextAuthOptions {
             email: session?.user?.email,
             name: session?.user?.name,
             image: session?.user?.image,
-            id: session?.user?.id
+            id: session?.user?.id,
           };
         }
 
@@ -72,25 +73,21 @@ export function getAuthOptions(): NextAuthOptions {
             sub: session?.user?.id,
             email: session?.user?.email,
             role: "authenticated",
-          }
-          session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+          };
+          session.supabaseAccessToken = jwt.sign(payload, signingSecret);
         }
         return session;
       },
       async signIn({ account, profile, email, user }: any) {
-        if (await getSession()) {
-          return true;
-        }
-        if (account.provider === 'spotify') {
+        if (account.provider === "spotify") {
           return true;
         }
 
         if (account.provider === "google") {
-          return profile.email_verified && profile.email.endsWith("@gmail.com")
+          return profile.email_verified && profile.email.endsWith("@gmail.com");
         }
-        return true // Do different verification for other providers that don't have `email_verified`
+        return true; // Do different verification for other providers that don't have `email_verified`
       },
-  
     },
     adapter: SupabaseAdapter({
       url: process.env.NEXT_PUBLIC_SUPABASE_URL as string,
@@ -105,8 +102,7 @@ export function getAuthOptions(): NextAuthOptions {
 }
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-const authOptions: NextAuthOptions = getAuthOptions();
-
+  const authOptions: NextAuthOptions = getAuthOptions();
 
   return await NextAuth(req, res, authOptions);
 }
