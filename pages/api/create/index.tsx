bd9bc49dcfcb0edeefd865fc2ddcefe37da51705
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-06-02 19:48:08 -0700

    testing prod

--- pages/api/create/index.tsx
+++ pages/api/create/index.tsx
@@ -1,52 +1,54 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { deployContractS } from 'lib/deployer';
-import { getServerSession } from 'next-auth/next';
-import { createClient } from '@supabase/supabase-js';
+import { NextApiRequest, NextApiResponse } from "next";
+import { deployContractS } from "lib/deployer";
+import { getServerSession } from "next-auth/next";
+import { createClient } from "@supabase/supabase-js";
 import { create } from "ipfs-http-client";
-import { Database } from 'types/supabase';
-import { writeFile } from 'fs/promises';
-import { getAuthOptions } from '../auth/[...nextauth]';
-const supabaseUrl = 'https://hlrcgzujgosmqgepcemj.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+import { Database } from "types/supabase";
+import { writeFile } from "fs/promises";
+import { getAuthOptions } from "../auth/[...nextauth]";
+const supabaseUrl = "https://hlrcgzujgosmqgepcemj.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-const name = 'Twinny Testing Vars';
-const tokenName = 'TTV';
+const name = "Twinny Testing Vars";
+const tokenName = "TTV";
 const startDate = Math.round(Date.now() / 1000);
 const endDate = 0;
-const contractUri = 'ipfs://testcontracturi';
+const contractUri = "ipfs://testcontracturi";
 const totalSupply = 500;
 
 export default async function handler(req: any, res: NextApiResponse) {
-  const authOptions = getAuthOptions()
+  const authOptions = getAuthOptions();
   // Get the user ID from the authentication provider (NextAuth.js)
   const session = await getServerSession(authOptions);
-  console.log(session, 'session for req')
+  console.log(session, "session for req");
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     let ipfsHash = null; // Declare the ipfsHash variable outside the try-catch block
 
     try {
       // Get the form data from the request body
       const collectibleData = {
         ...req.body,
-        userId: session?.id
+        userId: session?.id,
       };
 
       /// Upload Collection Data to IPFS
       try {
         ipfsHash = await uploadToIpfs(collectibleData);
-        console.log('Upload successful! IPFS hash:', ipfsHash);
+        console.log("Upload successful! IPFS hash:", ipfsHash);
 
         // Add Collection to Supabase
         const { data: collectible, error } = await supabase
-          .from('collectibles')
+          .from("collectibles")
           .insert(collectibleData)
-          .eq('userId', session?.id);
+          .eq("userId", session?.id);
 
         if (error) {
           console.error(error);
-          res.status(500).json({ success: false, error: 'Error inserting collectible' });
+          res
+            .status(500)
+            .json({ success: false, error: "Error inserting collectible" });
           return;
         }
 
@@ -55,29 +57,34 @@ export default async function handler(req: any, res: NextApiResponse) {
 
         // Deploy the contract using the form data
         const deployData = {
-          ...collectibleData,
-          ipfsHash: ipfsHash
+          name: collectibleData.name,
+          tokenName: collectibleData.name,
+          startDate: collectibleData.start_date,
+          endDate: collectibleData.end_date,
+          contractUri: ipfsHash,
+          totalSupply: collectibleData.total_collectibles,
         };
 
-        const contractAddress = await deployContractS({ deployData })
+        const contractAddress = await deployContractS({ deployData });
         // Return a JSON response with the contract address
         res.json({ success: true, contractAddress });
-
       } catch (error) {
-        console.error('Error uploading to IPFS:', error);
-        res.status(500).json({ success: false, error: 'Error uploading to IPFS' });
+        console.error("Error uploading to IPFS:", error);
+        res
+          .status(500)
+          .json({ success: false, error: "Error uploading to IPFS" });
         return;
       }
-
     } catch (error) {
       console.error(error);
-      res.status(500).json({ success: false, error: 'Error creating collectible' });
+      res
+        .status(500)
+        .json({ success: false, error: "Error creating collectible" });
     }
   } else {
-    res.status(404).json({ success: false, error: 'Endpoint not found' });
+    res.status(404).json({ success: false, error: "Endpoint not found" });
   }
 }
-
 
 const uploadToIpfs = async ({ collectibleData }: any) => {
   // Upload json file to IPFS
@@ -97,13 +104,19 @@ const uploadToIpfs = async ({ collectibleData }: any) => {
   // Upload json file to IPFS
   const jsonData = { collectibleData };
   const jsonContent = JSON.stringify(jsonData);
-  await writeFile(`/tmp/${collectibleData.name}-metadata.json`, jsonContent, 'utf8');
-  console.log(`metadata was successfully saved to ${collectibleData.name}-metadata.json file`);
+  await writeFile(
+    `/tmp/${collectibleData.name}-metadata.json`,
+    jsonContent,
+    "utf8"
+  );
+  console.log(
+    `metadata was successfully saved to ${collectibleData.name}-metadata.json file`
+  );
 
   // Upload audio file to IPFS
   const hashResult = await ipfs.add(jsonContent);
-  console.log(hashResult, 'hrs')
+  console.log(hashResult, "hrs");
   const hashUrl = `ipfs://${hashResult.path}`;
-  console.log(hashUrl, 'hashUrl')
+  console.log(hashUrl, "hashUrl");
   return { ipfsHash: hashUrl };
-}
+};
