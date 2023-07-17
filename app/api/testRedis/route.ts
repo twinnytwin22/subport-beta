a152859426198f8913812d65892c3261bdb90d0f
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-16 19:45:12 -0700

    Update route.ts

--- app/api/testRedis/route.ts
+++ app/api/testRedis/route.ts
@@ -4,7 +4,7 @@ import { redis } from "lib/redis/redis";
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
+const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_ANON_KEY!
 );
