b04190e43a8e59131897d964022301ef43c13d6d
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-17 17:25:39 -0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,13 @@
 // middleware.ts
-import { NextResponse } from "next/server";
 import { stackMiddlewares } from "utils/middlewares/stackMiddlewares";
 import { withApi } from "utils/middlewares/withApi";
 import { withHeaders } from "utils/middlewares/withHeaders";
 import { withLogging } from "utils/middlewares/withLogging";
 import { supaMiddleware } from "utils/middlewares/withSession";
 
-// const middlewares = [withLogging, withHeaders, withApi, supaMiddleware];
+const middlewares = [withLogging, withHeaders, withApi, supaMiddleware];
 
-export function middleware(request: Request) {
-  const origin = request.headers.get("origin");
-  console.log(origin);
-
-  const response = NextResponse.next();
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  response.headers.set("Access-Control-Max-Age", "86400");
-
-  console.log("Middleware!");
-  console.log(request.method);
-  console.log(request.url);
-
-  return response;
-}
-
+export default stackMiddlewares(middlewares);
 export const config = {
-  matcher: "/api/:path*",
+  matcher: ["/((?!_next/static|favicon.ico).*)"],
 };
