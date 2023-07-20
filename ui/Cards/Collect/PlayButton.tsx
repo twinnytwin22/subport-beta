4ef4b3c585521f4ec9017964dffd55ca05d4b146
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-20 12:21:32 -0700

    Update PlayButton.tsx

--- ui/Cards/Collect/PlayButton.tsx
+++ ui/Cards/Collect/PlayButton.tsx
@@ -1,27 +1,34 @@
 'use client'
 import { useAuthProvider } from 'app/context/auth'
 import { useSubportPlayer } from 'app/context/subport-player'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaPlayCircle, FaStopCircle } from 'react-icons/fa'
 
 function PlayButton({ props }: any) {
     const { play, isPlaying, stop, setAudioUrl, audioUrl } = useSubportPlayer()
     const { user } = useAuthProvider()
     const newAudioUrl = props?.metaData?.animation_url?.replace('ipfs://', 'https://gateway.ipfscdn.io/ipfs/')
-
+    const [mounted, setMounted] = React.useState(false);
+    React.useEffect(() => setMounted(true), []);
     const handlePlay = () => {
         if (audioUrl !== newAudioUrl) {
+            if (isPlaying) {
+                stop()
+                setAudioUrl()
+                play()
+            }
             setAudioUrl(newAudioUrl)
+            play()
         }
-        if (audioUrl) {
+        if (audioUrl === newAudioUrl) {
             play()
         }
     }
 
 
-    return user && newAudioUrl && (
+    return mounted && user && newAudioUrl && (
         <>
-            {isPlaying ? (
+            {isPlaying && audioUrl === newAudioUrl ? (
                 <div onClick={stop} className="absolute bottom-3 right-3 hover:scale-110 duration-300 ease-in-out transform -translate-x-1/2 -translate-y-1/2">
                     <FaStopCircle size={48} className="text-white opacity-80 cursor-pointer" />
                 </div>
