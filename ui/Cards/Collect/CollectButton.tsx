8efbe300b979ec851bbc51d9b642cace3e8d42dc
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-20 19:48:35 -0700

    update collect

--- ui/Cards/Collect/CollectButton.tsx
+++ ui/Cards/Collect/CollectButton.tsx
@@ -1,48 +1,75 @@
 'use client'
-import Link from 'next/link'
-import React from 'react'
-import { FaArrowRight } from 'react-icons/fa'
-import DropLinksTo from 'ui/Sections/Drop/DropLInks'
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import { FaArrowRight } from 'react-icons/fa';
+import DropLinksTo from 'ui/Sections/Drop/DropLInks';
 
 function CollectButton({ props, drop }: any) {
-    const [isOpen, setIsOpen] = React.useState(false)
+    const [isOpen, setIsOpen] = React.useState(false);
+
     const handleOpenCollectMenu = () => {
-        setIsOpen(true)
-    }
+        setIsOpen(true);
+    };
 
     const handleClose = () => {
-        setIsOpen(false)
-    }
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        const handleClick = (event: MouseEvent) => {
+            const targetElement = event.target as Element;
+            const isLinkOrChild = targetElement.closest('a');
+
+            if (!isLinkOrChild && !targetElement.closest('.collect-button')) {
+                setIsOpen(false);
+                return;
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener('mousedown', handleClick);
+        } else {
+            document.removeEventListener('mousedown', handleClick);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClick);
+        };
+    }, [isOpen]);
+
     return (
         <>
-            <div onClick={handleOpenCollectMenu}
-                //  href={`/drop/${drop?.slug}`}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            <div
+                onClick={handleOpenCollectMenu}
+                id='collect-button'
+                className="collect-button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Collect
             </div>
-            {isOpen &&
-                <div className='absolute bottom-24 right-4  bg-black w-fit mx-auto justify-center p-4 rounded-2xl items-center flex '>
+            {isOpen && (
+                <div className='absolute bottom-24 right-4 border border-zinc-200 dark:border-zinc-800 bg-white  dark:bg-zinc-900 w-fit mx-auto justify-center p-4 rounded-2xl items-center flex shadow-2xl '>
                     <div>
                         <Link href={`/drop/${drop.slug}`}>
                             <div
-                                className="text-white text-center w-full  focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-zinc-950 dark:hover:bg-zinc-500 focus:outline-none "
-
+                                className="text-white text-center w-full  focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 bg-zinc-950 hover:bg-zinc-500 focus:outline-none "
                             >
                                 <div className='flex space-x-2 mx-auto text-center items-center justify-center w-full'>Go to drop
                                     <FaArrowRight /></div>
-                            </div></Link>
+                            </div>
+                        </Link>
 
                         <DropLinksTo />
                         <button
-                            className="text-white text-center w-full  focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-zinc-950 dark:hover:bg-zinc-500 focus:outline-none "
-
-                            onClick={handleClose}>Close</button>
-
+                            className="text-zinc-900 dark:text-white text-center w-full border-2 dark:border-zinc-800 border-zinc-300 focus:ring-4 focus:ring-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-zinc-950 dark:hover:bg-zinc-700 focus:outline-none "
+                            onClick={handleClose}
+                        >
+                            Close
+                        </button>
                     </div>
-                </div>}
+                </div>
+            )}
         </>
-    )
+    );
 }
 
-export default CollectButton
+export default CollectButton;
