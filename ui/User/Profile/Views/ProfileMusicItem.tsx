b897932529ab2c51c52707ee398c38230e1f4a9e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-20 17:32:41 -0700

    yea budddy

--- ui/User/Profile/Views/ProfileMusicItem.tsx
+++ ui/User/Profile/Views/ProfileMusicItem.tsx
@@ -1,21 +1,26 @@
 'use client'
 import CollectCardMenu from "ui/Cards/Collect/CollectCardMenu"
 import { useRouter } from 'next/navigation'
+import PlayButton from "ui/Cards/Collect/PlayButton"
 
 
 
 const MusicItem = ({ drop, metaData, profile }: any) => {
     const imageHash = metaData?.image?.replace('ipfs://', 'https://gateway.ipfscdn.io/ipfs/') || metaData?.data.image?.replace('ipfs://', 'https://gateway.ipfscdn.io/ipfs/')
-    console.log("ITEM", drop, metaData)
     const router = useRouter()
+
+    const props = {
+        metaData
+    }
     return (
         <tr key={drop.name}
             className="border-b dark:border-zinc-600 hover:bg-zinc-100 dark:hover:bg-zinc-700 text-xs md:text-sm min-w-full ">
 
             <th scope="row"
                 className="flex items-center px-4 py-2  font-medium text-zinc-900 whitespace-nowrap dark:text-white">
-                <div className="block min-w-[40px] min-h-[40px] rounded-md bg-blue-300 w-fit mr-2">
-                    <img src={imageHash} className='object-cover  w-10 h-10 rounded-md' />
+                <div className="block min-w-[40px] min-h-[40px] rounded-md bg-blue-300 w-fit mr-2 relative place-items-center">
+                    <img src={imageHash} className='object-cover  w-10 h-10 rounded-md relative' />
+                    <div className="scale-50 mx-auto absolute justify-center left-5 mt-2.5  w-10 right-0"><PlayButton props={props} /></div>
                 </div>
 
             </th>
