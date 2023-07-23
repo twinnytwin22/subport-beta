fe56a61d56681778793eb2723de8a87415387b7a
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-23 03:26:29 -0700

    styling

--- ui/Cards/Collect/PlayButton.tsx
+++ ui/Cards/Collect/PlayButton.tsx
@@ -1,47 +1,67 @@
-'use client'
-import { useAuthProvider } from 'app/context/auth'
-import { useSubportPlayer } from 'app/context/subport-player'
-import React from 'react'
-import { FaPlayCircle, FaStopCircle } from 'react-icons/fa'
+"use client";
+import { useAuthProvider } from "app/context/auth";
+import { useSubportPlayer } from "app/context/subport-player";
+import React from "react";
+import { FaPlayCircle, FaStopCircle } from "react-icons/fa";
 
 function PlayButton({ props }: any) {
-    const { play, isPlaying, stop, setAudio, updateAudioUrl, setSongImage, updateImageUrl, audioUrl, useAudio, audioRef, pause, setAudioRef } = useSubportPlayer()
-    const { user } = useAuthProvider()
-    const newAudioUrl = props?.metaData?.animation_url?.replace('ipfs://', 'https://gateway.ipfscdn.io/ipfs/')
-    const newImageUrl = props?.metaData?.image?.replace('ipfs://', 'https://gateway.ipfscdn.io/ipfs/')
+    const { play, isPlaying, stop, updateAudioUrl, updateImageUrl, audioUrl } =
+        useSubportPlayer();
+    const { user } = useAuthProvider();
+    const newAudioUrl =
+        props?.metaData?.animation_url?.replace(
+            "ipfs://",
+            "https://gateway.ipfscdn.io/ipfs/"
+        );
+    const newImageUrl = props?.metaData?.image?.replace(
+        "ipfs://",
+        "https://gateway.ipfscdn.io/ipfs/"
+    );
     const [mounted, setMounted] = React.useState(false);
     React.useEffect(() => setMounted(true), []);
-
 
     const handlePlay = async () => {
         if (audioUrl !== newAudioUrl) {
             if (isPlaying) {
-                stop()
-
+                stop();
             }
-            updateImageUrl(newImageUrl)
-            updateAudioUrl(newAudioUrl)
+            updateImageUrl(newImageUrl);
+            updateAudioUrl(newAudioUrl);
             await new Promise((resolve) => setTimeout(resolve, 1000));
-            play()
+            play();
         } else {
-            play()
+            play();
         }
+    };
 
-    }
-
-
-    return mounted && user && newAudioUrl && (
-        <>
-            {isPlaying && audioUrl === newAudioUrl ? (
-                <div onClick={stop} className="hover:scale-110 duration-300 ease-in-out ">
-                    <FaStopCircle size={48} className="text-white opacity-80 cursor-pointer" />
-                </div>
-            ) : (
-                <div onClick={handlePlay} className="hover:scale-110 duration-300 ease-in-out transform ">
-                    <FaPlayCircle size={48} className="text-white opacity-80 cursor-pointer" />
-                </div>
-            )}
-        </>
+    return (
+        mounted &&
+        user &&
+        newAudioUrl && (
+            <>
+                {isPlaying && audioUrl === newAudioUrl ? (
+                    <div
+                        onClick={stop}
+                        className="hover:scale-110 duration-300 ease-in-out "
+                    >
+                        <FaStopCircle
+                            size={48}
+                            className="text-white opacity-80 cursor-pointer"
+                        />
+                    </div>
+                ) : (
+                    <div
+                        onClick={handlePlay}
+                        className="hover:scale-110 duration-300 ease-in-out transform "
+                    >
+                        <FaPlayCircle
+                            size={48}
+                            className="text-white opacity-80 cursor-pointer"
+                        />
+                    </div>
+                )}
+            </>
+        )
     );
 }
 
--- ui/Misc/SearchBar.tsx
+++ ui/Misc/SearchBar.tsx
@@ -42,7 +42,7 @@ const SearchBar = () => {
                 <input
                     type="text"
                     id="simple-search"
-                    className="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:border-zinc-700 dark:bg-zinc-900 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search"
                     value={searchTerm}
                     onChange={(event) => setSearchTerm(event.target.value)}
