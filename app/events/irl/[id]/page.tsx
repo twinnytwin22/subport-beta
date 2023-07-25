0fea992b08f05fb7d122762ee66dec21cc17b60e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-25 16:09:51 -0700

    Update page.tsx

--- app/events/irl/[id]/page.tsx
+++ app/events/irl/[id]/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { headers } from "next/headers";
-import { notFound } from "next/navigation";
 import { reformatDate } from "lib/hooks/formatDate";
-import { FaBookmark, FaFlag } from "react-icons/fa";
-import MinIEventCard from "ui/Cards/Events/MinIEventCard";
+import { FaBookmark, FaFlag, FaMapPin } from "react-icons/fa";
+import Image from "next/image";
+import EventHeader from "ui/Sections/Events/EventHeader";
+import EventDetails from "ui/Sections/Events/EventDetails";
+import EventGoogleMap from "ui/Sections/Events/EventMap";
+import { checkUser } from "utils/database";
+import { downloadImage } from "lib/hooks/downloadImage";
+import { useImagePath } from "lib/constants";
+import { FollowButton } from "ui/Buttons/FollowButton";
+import EventOrganizer from "ui/Sections/Events/EventOrganizer";
+import EventTicketContainer from "ui/Sections/Events/EventTicketContainer";
 export default async function Page({
     params,
 }: {
@@ -24,13 +32,36 @@ export default async function Page({
     const event = data.find((eventItem: any) => eventItem.slug === id);
 
     if (event) {
-        const inputDate = event.date;
-        const Dates = reformatDate(inputDate);
-        console.log(Dates);
+        const user = await checkUser({ id: event.user_id });
+        if (user) {
+            const inputDate = event.date;
+            const Dates = reformatDate(inputDate);
+            console.log(Dates);
+            const image = event.image?.replace(
+                "ipfs://",
+                "https://gateway.ipfscdn.io/ipfs/"
+            );
+            const artistImage = useImagePath(user.profile?.avatar_url);
 
-        return (
-            <MinIEventCard Dates={Dates} event={event} />
-        );
+            return user.profile && (
+                <div className="min-h-screen h-full mb-40">
+                    {/* Header with blurred image */}
+                    <EventHeader image={image} Dates={Dates} event={event} />
+                    <div className="flex flex-col md:flex-row w-full  space-x-8">
+                        <div className="w-full">
+                            <EventOrganizer user={user} artistImage={artistImage} />
+                            <EventDetails image={image} Dates={Dates} event={event} />
+                        </div>
+                        <div className=" col-span-1 mt-8 w-fit rounded-md  relative">
+                            <EventTicketContainer event={event} />
+
+                            <EventGoogleMap lat={0} lng={0} />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        return null;
     }
-    return null;
 }
