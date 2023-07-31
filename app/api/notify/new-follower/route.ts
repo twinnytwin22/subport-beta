6913a41eb862425b6624fe920c82b99e272af603
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-07-30 18:22:36 -0700

    Update route.ts

--- app/api/notify/new-follower/route.ts
+++ app/api/notify/new-follower/route.ts
@@ -1,51 +1,22 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-let data = { record: { following_id: "", follower_id: "", created_at: "" } };
-const supabaseApi = createRouteHandlerClient({ cookies });
+
+let data = "";
 
 export async function GET(req: Request) {
   return NextResponse.json({
     status: 200,
-    following_id: data.record.following_id || null,
-    follower_id: data.record?.follower_id || null,
-    created_id: data.record.created_at || null,
+    data,
   });
 }
 
 export async function POST(request: Request) {
   const req = await request.json();
-  const { following_id, follower_id, created_at } = req.data;
 
-  // Insert the notification into the Supabase table
-  try {
-    const { data, error } = await supabaseApi.from("notifications").insert([
-      {
-        user_id: following_id,
-        message: "You have a new follower.",
-        created_at,
-      },
-    ]);
+  data = req;
 
-    if (error) {
-      // Handle the error if the insertion fails
-      console.error("Error inserting notification:", error);
-      return NextResponse.json({
-        status: 500,
-        error: "Failed to insert notification.",
-      });
-    }
-
-    return NextResponse.json({
-      status: 200,
-      data,
-      message: "New Follower for user",
-    });
-  } catch (error) {
-    console.error("Error inserting notification:", error);
-    return NextResponse.json({
-      status: 500,
-      error: "Failed to insert notification.",
-    });
-  }
+  return NextResponse.json({
+    status: 200,
+    data,
+    message: "New Follower for",
+  });
 }
