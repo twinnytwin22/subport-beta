cecf22dbdbb671f1f0ac655da5e2b4466c156307
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-08-27 19:23:18 -0700

    grabbing code snippets

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { supabaseAuth } from 'lib/constants';
-import { supabaseAdmin } from 'lib/providers/supabase/supabase-lib-admin'
 import { NextResponse } from 'next/server'
 export const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL!;
 export const supabaseKey =  process.env.SUPABASE_ANON_KEY|| process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
+  const cookies = req.cookies.getAll()
   const res = NextResponse.next()
- // const supabase = createMiddlewareClient({ req, res }, {supabaseKey, supabaseUrl})
- 
- const {data: session }= await supabaseAuth.auth.getSession()
+ const supabase = createMiddlewareClient({ req, res }, {supabaseKey, supabaseUrl})
+ console.log(cookies, "ALL COOKIES IN MIDDLEWARE")
+ const {data: session }= await supabase.auth.getSession()
  console.log(session, 'MIDDLEWARE SESSION')
   return res
 }
