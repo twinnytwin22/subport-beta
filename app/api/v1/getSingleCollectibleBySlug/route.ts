6a8e5d08ac4fecde23b03690a25c57774fabb38f
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-21 17:39:14 -0700

    Update route.ts

--- app/api/v1/getSingleCollectibleBySlug/route.ts
+++ app/api/v1/getSingleCollectibleBySlug/route.ts
@@ -5,37 +5,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 0;
 export const dynamic = 'force-dynamic'
+
 // Promisify Redis get and set methods
 
-export async function GET(req: NextRequest) {
-  if (req.method !== 'GET') {
-    return NextResponse.json('error: Method Not Allowed', { status: 405 });
-  }
-  const { searchParams } = new URL(req.url);
-  const slug = searchParams.get("slug");
-
-  const cacheKey = "drops_cache"; // Specify a cache key
-  let cachedData = undefined;
-  // Check if the response is available in Redis cache
-  const cachedResponse = await redisGet(cacheKey);
-  if (cachedResponse) {
-    cachedData = JSON.parse(cachedResponse);
-    const existingData = cachedData.dropsWithMetaData;
-    const existingDrop = existingData.find(
-      (drop: any) => drop.drop.slug === slug
-    );
-
-    if (existingDrop) {
-      return NextResponse.json(existingDrop);
+export async function GET(req: NextRequest){
+  try {
+    if (req.method !== 'GET') {
+      return NextResponse.json('error: Method Not Allowed', { status: 405 });
     }
+    const { searchParams } = new URL(req.url);
+    const slug = searchParams.get("slug");
 
- 
-  } else {
+    const cacheKey = "drops_cache"; // Specify a cache key
+    let cachedData = undefined;
+    // Check if the response is available in Redis cache
+    const cachedResponse = await redisGet(cacheKey);
+    if (cachedResponse) {
+      cachedData = JSON.parse(cachedResponse);
+      const existingData = cachedData.dropsWithMetaData;
+      const existingDrop = existingData.find(
+        (drop: any) => drop.drop.slug === slug
+      );
 
-    if (!slug) {
-      return NextResponse.json({ 'Error': 'Missing or empty slug', 'status': 400 });
-    }
-    try {
+      if (existingDrop) {
+        return NextResponse.json(existingDrop);
+      }
+    } else {
+      if (!slug) {
+        return NextResponse.json({ 'Error': 'Missing or empty slug', 'status': 400 });
+      }
+
       const { data: drop, error } = await supabaseApi
         .from("drops")
         .select("*")
@@ -56,10 +55,11 @@ export async function GET(req: NextRequest) {
       } else if (error) {
         return NextResponse.json({ error });
       }
-    } catch (error) {
-      console.error("Error fetching single contract URI:", error);
     }
 
     return NextResponse.json({ error: "Not Found", drop: null });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
