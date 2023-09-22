5a6fb661b4e281a9c53733e813fb5aa6b35c07dc
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-21 17:33:53 -0700

    Update supabase-server.tsx

--- lib/providers/supabase/supabase-server.tsx
+++ lib/providers/supabase/supabase-server.tsx
@@ -1,7 +1,9 @@
+'use-server'
 import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from 'types/database.types';
-export const createServerClient = () => {
+import { cache } from 'react';
+export const createServerClient = cache(() => {
 const cookieStore = cookies()
 return createServerComponentClient<Database>({ cookies: () => cookieStore })
-}
+})
