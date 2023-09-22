d11d96d3ec421c310d829f2f7b8e76ac28576fc3
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-21 17:42:06 -0700

    Update route.ts

--- app/api/v1/testRedis/route.ts
+++ app/api/v1/testRedis/route.ts
@@ -1,23 +1,30 @@
-// Import necessary dependencies
 import { NextRequest, NextResponse } from "next/server";
-import { redis, redisGet, redisSet } from "lib/redis/redis";
+import { redisGet, redisSet } from "lib/redis/redis";
 
-// Create a Supabase
-
-// Promisify Redis get and set methods
+// Define a cache key
+const cacheKey = "drops_cache";
 
 // Define the route handler function
-export async function GET(req:NextRequest) {
-  if (req.method !== 'GET') {
-    return NextResponse.json('error: Method Not Allowed', { status: 405 });
-  }
-  const cacheKey = "drops_cache"; // Specify a cache key
+export async function GET(req: NextRequest): Promise<NextResponse> {
+  try {
+    // Check if the request method is GET
+    if (req.method !== 'GET') {
+      return NextResponse.json('error: Method Not Allowed', { status: 405 });
+    }
 
-  // Check if the response is available in Redis cache
-  const cachedResponse = await redisGet(cacheKey);
-  if (cachedResponse) {
-    return NextResponse.json(JSON.parse(cachedResponse));
-  } else {
-    return false;
+    // Check if the response is available in Redis cache
+    const cachedResponse = await redisGet(cacheKey);
+
+    if (cachedResponse) {
+      // If found in cache, return the cached response
+      return NextResponse.json(JSON.parse(cachedResponse));
+    } else {
+      // If not found in cache, return a response indicating it's not in cache
+      return NextResponse.json({ message: 'Data not found in cache' });
+    }
+  } catch (error) {
+    // Handle any errors that may occur
+    console.error("An error occurred:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
