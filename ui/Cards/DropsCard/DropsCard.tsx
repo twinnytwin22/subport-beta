fada8b0045171147dbf145ba8e7fbd82ac04653e
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-09-21 19:15:59 -0700

    Update DropsCard.tsx

--- ui/Cards/DropsCard/DropsCard.tsx
+++ ui/Cards/DropsCard/DropsCard.tsx
@@ -6,22 +6,22 @@ function DropsCard({ drop }: { drop: any }) {
     const image = useIpfsImage(drop.metaData.image)
   return (
     <div className=" max-w-sm min-w-max w-full overflow-hidden  m-4 bg-white border border-zinc-200 rounded-md dark:bg-black dark:border-zinc-700 shadow-md shadow-zinc-200 dark:shadow-zinc-900 ">
+           <div className='relative  object-cover aspect-square w-72 '>
+
+<Image
+  width={400}
+  height={400}
+  src={image}
+  alt={drop.drop.title}
+  className="aspect-square object-cover w-72 h-72 relative" />
+</div>  
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{drop.drop.title}</div>
-        <p className="text-zinc-700">Genre: {drop.drop.genre}</p>
+        <p className=" text-zinc-700 dark:text-zinc-300 text-xs">Genre: {drop.drop.genre}</p>
         <p className="text-zinc-700 hidden">Keywords: {drop.drop.keywords.join(', ')}</p>
         <p className="text-zinc-700 hidden">Spotify URI: <a href={drop.drop.spotify_uri} target="_blank" rel="noopener noreferrer">{drop.drop.spotify_uri}</a></p>
       </div>
-      <div className="px-6 py-4">
-      <div className='relative  object-cover aspect-square w-72 '>
-
-        <Image
-          width={400}
-          height={400}
-          src={image}
-          alt={drop.drop.title}
-          className="aspect-square object-cover w-72 h-72 relative" />
-      </div>      </div>
+    
     </div>
   );
 }
