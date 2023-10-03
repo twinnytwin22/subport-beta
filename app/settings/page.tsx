c99a1069001f3c8deba77aba40fee13cd6fd9376
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-02 20:06:24 -0700

    Update page.tsx

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,10 +1,11 @@
-import { getSession } from "lib/providers/supabase/supabase-client-server";
+import { supabase } from "lib/constants";
 import React from "react";
 import SettingsPage, { SettingsPageSmall } from "ui/User/Account";
 
 async function Page() {
-const session = await getSession() 
-console.log(session)
+const session = await supabase.auth.getSession()
+
+//console.log(session)
     return session && (
         <section>
             <div className="hidden md:block">
