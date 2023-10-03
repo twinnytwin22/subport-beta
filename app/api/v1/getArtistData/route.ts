93d9fec9e6f4e9b778259dee76829ffb8901eb77
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-10-02 19:36:54 -0700

    Update route.ts

--- app/api/v1/getArtistData/route.ts
+++ app/api/v1/getArtistData/route.ts
@@ -3,30 +3,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from 'lib/constants';
 export async function GET(req:NextRequest) {
-
-  if (req.method !== 'GET') {
-    return NextResponse.json('error: Method Not Allowed', { status: 405 });
-  }
-
-  const { searchParams } = new URL(req.url);
-  const userId = searchParams.get('userId')
-  if (userId) {
-  const { data: settings, error } = await supabase.from('artist_settings').select('spotify_url').eq('user_id', userId).maybeSingle();
-
-  if (error) {
-    console.error(error);
-    return NextResponse.json({'Error fetching data': 500});
-  }
-
-  //const filteredProfiles = users?.filter((user) => user.drops.length > 1);
-
-  if(settings){
-    return NextResponse.json({ data: settings });
-
-  }
-
-  return new Error('Error fetching Artist Data')
-}
-return new Error('Error fetching Artist Data')
-
-}
+    if (req.method !== 'GET') {
+        return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
+      }
+    
+      const { searchParams } = new URL(req.url);
+      const userId = searchParams.get('userId');
+    
+      if (!userId) {
+        return NextResponse.json({ error: 'User ID is missing' }, { status: 400 });
+      }
+    
+      try {
+        const { data: settings, error } = await supabase
+          .from('artist_settings')
+          .select('spotify_url')
+          .eq('user_id', userId)
+          .maybeSingle();
+    
+        if (error) {
+          console.error(error);
+          return NextResponse.json({ error: 'Error fetching data' }, { status: 500 });
+        }
+    
+        if (settings) {
+          return NextResponse.json({ data: settings });
+        } else {
+          return NextResponse.json({ error: 'Artist data not found' }, { status: 404 });
+        }
+      } catch (err) {
+        console.error(err);
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+      }
+    }
