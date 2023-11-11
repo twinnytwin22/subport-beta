840b226dd987e8b8f8daff6e6beee5bd3e0d3df7
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-11-11 11:03:30 -0700

    Update ExploreQuery.tsx

--- ui/Sections/Explore/ExploreQuery.tsx
+++ ui/Sections/Explore/ExploreQuery.tsx
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 
 // Use the useQuery hook to fetch and cache events
 const EventsQuery = (fetchEvents: any) => {
-  return useQuery(['events', fetchEvents]);
+  return useQuery({queryKey:['events', fetchEvents]});
 };
 
 export default EventsQuery;
